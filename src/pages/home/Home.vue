bb29d8f767b8b999d5622eaca47ba99df7d734be
bojueLi <1028643774@qq.com>
2018-08-20 21:48:23 +0800

    first commit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <HomeTop></HomeTop>
+    </div>
+</template>
+
+<script>
+import HomeTop from './components/HomeTop'
+export default {
+    name: 'Home',
+    components:{
+        HomeTop
+    }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/pages/home/components/HomeTop.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <div class="header-left">返回</div>
+        <div class="header-input">内容</div>
+        <div class="header-right">城市</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeTop'
+}
+</script>
+<style lang="stylus" scoped>
+    .header
+        height .86rem
+        display flex
+        background #00b4d4
+    .header-left
+        width .64rem
+        float left
+    .header-input
+        flex 1
+    .header-right
+        width 1.24rem
+        float left
+
+</style>
+
